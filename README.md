eb46fde520919a961dc24506c9e06c6e5f623a31
Melanie <melanieuhrich13@gmail.com>
2021-04-02 21:51:26 -0500

    updated

--- README.md
+++ README.md
@@ -1,17 +1,45 @@
 # Homework-3
 
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN prompted for character types to include in the password
-THEN I choose lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
-WHEN all prompts are answered
-THEN a password is generated that matches the selected criteria
-WHEN the password is generated
-THEN the password is either displayed in an alert or written to the page
+# Purpose of the Assignment 
+
+Generate a random password that allows the user to select the length and to choose whether or not to include lowercase characters, uppercase characters, numeric characters, and special characters.
+
+# What I Did and Why 
+
+Line 1: Creates the 'generateBtn' variable and links it with the the 'Generate Password' button. 
+
+Lines 2-5: Creates and defines variables for each group of selectable characters.
+
+Line 6: Creates 'selectionString' variable and defines it as an empty string. 
+
+Lines 8-12: Declares the 'getRandomInt' function, which will take random integers from each variable used to generate the password.
+
+Line 14: Declares the 'writePassword' function. 
+
+Line 16: Establishes an empty selectionString again so that it resets itself each time a new password is generated. 
+
+Line 18: Creates the 'passwordText' variable and links it with the password display text area.
+
+Line 20: Creates the 'length' variable, generates the prompt, and defines the length according to the answer to the prompt. 
+
+Lines 22-25: Creates an alert to be displayed in the event that the length entered by the user is less than 8. If an acceptable length is selected, it moves on.
+
+Lines 27-30: Creates an alert to be displayed in the event that the length entered by the user is more than 128. If an acceptable length is selected, it moves on.
+
+Lines 32-35: Creates variables to establish included characters, generates confirm/deny prompts, and defines the prompts as true or false according to the answer selected by the user. 
+
+Lines 39-61: If the prompt is defined as true, adds the applicable characters to the string and moves onto to the next prompt. If it is false, keeps the string as is and moves onto the next prompt. 
+
+Line 63: Tells the password display text area to include the defined 'selectionString' and 'length' variables. Calls the 'generate' function to make this happen. The 'writePassword' function has been declared and defined, but not called. 
+
+Line 69: Declares the 'generate' function. 
+
+Line 70: Within the function, creates the password variable and defines it as an empty string. 
+
+Line 71: Within the function, creates a for loop so that the function cycles through all necessary variables. 
+
+Line 72: Defines the password variable as itself in addition to the selection string and calls the 'getRandomInt' function, 0 being the minimum position and the length of the selection string being the maximum position. 
+
+Line 74: Returns the password. The 'generate' function has been completed.
+
+Line 78: Tells the 'Generate Password' button to listen for a click, and on that click to call the 'writePassword' function. 
--- script.js
+++ script.js
@@ -1,36 +1,78 @@
-// Assignment Code
 var generateBtn = document.querySelector("#generate"); //generateBtn is now a reference to the Generate Password Button
+var lowChar = "abcdefghijklmnopqrstuvwxyz";
+var upChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var num = "1234567890";
+var spec = "!#$%&()*+,-./:;<=>?@[\]^_`{|}~"; 
+var selectionString = "";
 
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); 
+}
 // Write password to the #password input
 function writePassword() { //declaring a function
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password"); //passwordText is now a reference to the textarea tag in the html; making it so that you can manipulate it in the html
 
-  passwordText.value = "password"; //"password" is the value of that text area 
+    selectionString = "";
+    
+  var passwordText = document.querySelector("#password"); //passwordText is now a reference to the textarea tag in the html; making it so that you can manipulate it in the html
 
   var length = parseInt(prompt("Please enter the length of the password"));
 
-if (length < 8) {
-    console.log("Password length should be greater than 8 characters");
-    alert("Password length should be greater than 8 characters");
+    if (length < 8) {
+        console.log("Password length should be greater than 8 characters");
+        alert("Password length should be greater than 8 characters");
+    }
+
+    if (length > 128) {
+        console.log("Password length should be less than 128 characters");
+        alert("Password length should be less than 128 characters");
+    }
+
+    var lowCharCon = confirm("Would you like to include lowercase characters?"); 
+    var upCharCon = confirm("Would you like to include uppercase characters?"); 
+    var numCon = confirm("Would you like to include numbers?"); 
+    var specCon = confirm("Would you like to include special characters?"); 
+
+    
+
+    if (lowCharCon === true) {
+        selectionString = selectionString + lowChar; 
+    } else {
+        selectionString = selectionString;
+    }; 
+    
+    if (upCharCon  === true) {
+        selectionString = selectionString + upChar;
+    } else {
+        selectionString = selectionString;
+    }; 
+
+    if (numCon === true) {
+        selectionString = selectionString + num
+    } else {
+        selectionString = selectionString;
+    }; 
+    
+    if (specCon === true) {
+        selectionString = selectionString + spec
+    } else {
+        selectionString = selectionString;
+    };
+
+    passwordText.value = generate(selectionString, length); //"password" is the value of that text area 
+
+
 }
 
-if (length > 128) {
-    console.log("Password length should be less than 128 characters");
-    alert("Password length should be less than 128 characters");
-}
-
-    var characters = prompt
-
+//We are defining the generate function here, now it takes two parameters
+function generate(selectionString, length) {
+    var password = ""; 
+    for(var i=0; i<length; i++){ 
+        password += selectionString[getRandomInt(0, selectionString.length)];
+    }
+    return password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); //calling generateBtn is clicked, it calls the writePassword function 
-
-function generatePassword() {
-    //your task is to generate a string with the required criteria 
-    return "password"
-    //Step 1: look up how to generate a single character in javascript
-    //Step 2: say we now have a length variable, then run a for loop and it should iterate LENGTH number of times and run the generate character function in it 
-    //Step 3: return the resulting string
-}
